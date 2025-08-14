0a7338d130ea43a10ab6108ce6fd1b67151258c6
Finic! <hectorbatterby@gmail.com>
2025-08-14 14:56:35 +0100

    using a struct for the player instead of 'SDL_rect'

--- main.cpp
+++ main.cpp
@@ -1,34 +1,57 @@
 #include <cstdint>
-#include <iostream>
+#include <stdio.h>
 #include <SDL2/SDL.h>
 #include "include/definitions.hpp"
 
-Uint64 last_tick = 0;
-Uint64 current_tick = 0;
-float delta_time;
+const int FPS = 60;
+const int frameDelay = 1000 / FPS; // 1000 / 60 (fps)
+
+Uint32 framestart;
+int frametime;
+
+struct Player
+{
+	int x, y;
+	int width;
+	int height;
+
+	int speed;
+	int health;
+};
+
+Player createPlayer(int x, int y)
+{
+	Player player;
+	player.x = x;
+	player.y = y;
+	player.width = 20;
+	player.height = 20;
+	player.speed = 5;
+	player.health = 100;
+	return player;
+}
 
 void initSDL()
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0);
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
-		std::cout << "Error initializing SDL! : " << SDL_GetError() << std::endl;
+		printf("Error initialising SDL : %s", SDL_GetError());
 	}
 }
 
-bool CreateWinRen(SDL_Window*& window, SDL_Renderer*& renderer) 
+bool CreateWinRen(SDL_Window *&window, SDL_Renderer *&renderer)
 {
 	window = SDL_CreateWindow(
-			"fisch",
-			SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED,
-			WINHIGHT,
-			WINWIDTH,
-			0
-	);
-	
+		"fisch",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		WINHIGHT,
+		WINWIDTH,
+		0);
+
 	if (!window)
 	{
-		std::cerr << "Error window failed te be created : " << SDL_GetError() << std::endl;
+		printf("error creating window : %s", SDL_GetError());
 		return false;
 	}
 
@@ -36,17 +59,17 @@ bool CreateWinRen(SDL_Window*& window, SDL_Renderer*& renderer)
 
 	if (!renderer)
 	{
-		std::cerr << "Error renderer failed te be created : " << SDL_GetError() << std::endl;
+		printf("error creating renderer : %s", SDL_GetError());
 		return false;
 	}
 
 	return true;
 }
 
-bool update(SDL_Rect &player, int &PvelocityX, int &PvelocityY)
+bool update(Player &player)
 {
-	SDL_Event event;	
-	
+	SDL_Event event;
+
 	while (SDL_PollEvent(&event))
 	{
 		if (event.type == SDL_QUIT)
@@ -56,113 +79,84 @@ bool update(SDL_Rect &player, int &PvelocityX, int &PvelocityY)
 
 		if (event.type == SDL_KEYDOWN)
 		{
-			switch (event.key.keysym.sym) {
-				case SDLK_ESCAPE:
-					return false;
-					break;
-				case SDLK_w:
-					PvelocityY -= 2;	
-					break;
-				case SDLK_s:
-					PvelocityY += 2;
-					break;
-				case SDLK_a:
-					PvelocityX -= 2;
-					break;
-				case SDLK_d:
-					PvelocityX += 2;
-					break;
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				return false;
+				break;
+			case SDLK_w:
+				player.y -= 2;
+				break;
+			case SDLK_s:
+				player.y += 2;
+				break;
+			case SDLK_a:
+				player.x -= 2;
+				break;
+			case SDLK_d:
+				player.x += 2;
+				break;
 			}
 		}
-
 	}
 	return true;
 }
 
-void drawSky(SDL_Renderer* renderer, int r, int g, int b, int x, int y)
+void drawSky(SDL_Renderer *renderer, int r, int g, int b, int x, int y)
 {
-	SDL_SetRenderDrawColor(renderer, 255,255,255, 255);
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_RenderClear(renderer);
 }
 
-void drawRect(SDL_Renderer* renderer, int r, int g, int b, SDL_Rect player)
+void drawRect(SDL_Renderer *renderer, int r, int g, int b, Player &player)
 {
 	SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-	SDL_RenderFillRect(renderer, &player);
+	SDL_Rect rect = {player.x, player.y, player.width, player.height};
+	SDL_RenderFillRect(renderer, &rect);
 	SDL_RenderPresent(renderer);
 }
 
-int main(int argc, char* argv[])
+void BoundryCheck(Player &player)
 {
-	SDL_Window* window;
-	SDL_Renderer* renderer;
+	if (player.x + player.width < 0)
+	{
+		player.x = 0;
+	}
+	if (player.x + player.width > 800)
+	{
+		player.x = (800 - player.width);
+	}
+	if (player.y + player.height < 0)
+	{
+		player.y = 0;
+	}
+	if (player.y + player.height > 600)
+	{
+		player.y = (600 - player.height);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
 
 	initSDL();
 	CreateWinRen(window, renderer);
-	
-		
-	// player vars - what did u think it fucking says it basically
-	SDL_Rect player;
-	player.x = 200;
-	player.y = 200;
-	player.w = 50;
-	player.h = 50;
-	int PvelocityX = 0;
-	int PvelocityY = 0;
-	int PvelocityCap = 10;
 
+	Player player = createPlayer(200, 200);
 
 	while (true)
 	{
-		last_tick = current_tick;
-		current_tick = SDL_GetTicks();
-		delta_time = (current_tick - last_tick) / 1000.0f;
-		
-		if (player.x + player.w < 0) 
+		if (!update(player))
 		{
-			PvelocityX -= PvelocityX;
-			player.x = 0;
-		}
-		if (player.x + player.w > 800)
-		{
-			PvelocityX -= PvelocityX;
-			player.x = (800 - player.w);
-		}
-		if (player.y + player.h < 0)
-		{
-			PvelocityY -= PvelocityY;
-			player.y = 0;
-		}
-		if (player.y + player.h > 600)
-		{
-			PvelocityY -= PvelocityY;
-			player.y = (600 - player.h);
+			break;
 		}
 
-		if (update(player, PvelocityX, PvelocityY) == false)
-		{
-			return false;
-		}	
+		BoundryCheck(player);
 
-		player.x += PvelocityX;
-		player.y += PvelocityY;
-
-		if (PvelocityX >= PvelocityCap)
-		{
-			PvelocityX = PvelocityCap;
-		}
-
-		if (PvelocityX >= PvelocityCap)
-		{
-			PvelocityY = PvelocityCap;
-		}
-
-	
-	
 		drawSky(renderer, 110, 40, 240, 299, 222);
 		drawRect(renderer, 255, 0, 0, player); // make sure this is at the end (renderpresent)
-		
-		SDL_Delay(delta_time);
 	}
 
 	SDL_DestroyWindow(window);
